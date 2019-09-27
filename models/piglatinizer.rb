a743c888801be95c857fdde9c86cc8ca550be0a9
Ranika Williams <rnkprice07@gmail.com>
2019-09-27 20:29:07 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,14 +1,11 @@
 class PigLatinizer
 
   def piglatinize(user_phrase)
-    @new_word = user_phrase.split("")
-    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-    if vowels.include?(@new_word[0])
-      latinized_word = begins_with_vowel.join("")
+    if user_phrase.split(" ").length == 1
+      word(user_phrase)
     else
-      latinized_word = begins_with_consonant.join("")
+      to_pig_latin(user_phrase)
     end
-    latinized_word
   end
 
   def word(phrase)
